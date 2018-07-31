b6bba436884e2f1216f983b66522e2ea83e81374
Ralph Jansen <ralph.jansen@outlook.com>
2018-07-31 22:54:10 +0200

    Format script

--- Scripts/Set-FunctionAppAsOwnerApplication.ps1
+++ Scripts/Set-FunctionAppAsOwnerApplication.ps1
@@ -2,20 +2,15 @@ $applicationName = "AppKeyRotator"
 $environment = "Test"
 
 $functionAppName = $applicationName + $environment
+$appObjectIdThatNeedsRotation = "83d86c42-6567-49e0-ab8c-e40848205883"
 
 #Get MSI of rotator function app
 Get-AzureRmADServicePrincipal -SearchString $functionAppName | Where-Object { $_.DisplayName -eq $functionAppName }
 $msiObjectId = $(Get-AzureRmADServicePrincipal -SearchString $functionAppName | Where-Object { $_.DisplayName -eq $functionAppName }).Id
 
-
-# Get application that needs key rotation
-$appObjectIdThatNeedsRotation = "83d86c42-6567-49e0-ab8c-e40848205883"
-
-
-#Get-AzureADApplication -ObjectId $appObjectIdThatNeedsRotation
+# Get application and service principal
 Get-AzureRmADApplication -ObjectId $appObjectIdThatNeedsRotation
 Get-AzureRmADApplication -ObjectId $appObjectIdThatNeedsRotation | Get-AzureRmADServicePrincipal
-
 
 #Add MSI as owner of the application
 Get-AzureADApplicationOwner -ObjectId $appObjectIdThatNeedsRotation
