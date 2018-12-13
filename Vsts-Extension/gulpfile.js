2389e8ad6d1780f1def2520a4f28c68e899232c7
Ralph Jansen <ralph.jansen@outlook.com>
2018-12-13 10:23:15 +0100

    Create a clean and reset option

--- Vsts-Extension/gulpfile.js
+++ Vsts-Extension/gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var HubRegistry = require('gulp-hub');
+var del = require('del');
 
 /* load some files into the registry */
 var hub = new HubRegistry(['gulp-tasks/**/*.js']);
@@ -11,9 +12,19 @@ gulp.registry(hub);
 
 
 function buildReadmeFile() {
-    console.log('Copy the readme file to the root of the extension');
-    return gulp.src('../Readme.md')
-      .pipe(gulp.dest('./'));
+  console.log('Copy the readme file to the root of the extension');
+  return gulp.src('../Readme.md')
+    .pipe(gulp.dest('./'));
 }
 
+function cleanReadmeFile() {
+  console.log('Delete the readme file from the root of the extension');
+  return del([
+    './Readme.md',
+  ]);
+}
+
+gulp.task('clean', gulp.series(cleanReadmeFile, gulp.parallel('clean')));
 gulp.task('default', gulp.series(buildReadmeFile, gulp.parallel('build')));
+gulp.task('build', gulp.series(buildReadmeFile, gulp.parallel('build')));
+gulp.task('reset', gulp.series(cleanReadmeFile, buildReadmeFile, gulp.parallel('reset')));
