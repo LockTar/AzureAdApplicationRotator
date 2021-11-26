aab55ee8303bb5d00da98fdb3af5d23efb9bacf6
Ralph Jansen <ralph.jansen@outlook.com>
2021-11-26 12:45:47 +0100

    Fix script

--- scripts/test/Set-FunctionAppAsOwnerApplication.ps1
+++ scripts/test/Set-FunctionAppAsOwnerApplication.ps1
@@ -1,14 +1,22 @@
 $applicationName = "AppKeyRotator"
 $environment = "Test"
 
-$functionAppName = $applicationName + $environment
+$functionAppName = $applicationName + $environment + "-AppKeyRotator"
 $appObjectIdThatNeedsRotation = "83d86c42-6567-49e0-ab8c-e40848205883"
 
+Write-Host "Your Function app is $functionAppName"
+
 #Get MSI of rotator function app
+Write-Host "Find Service Principal (MSI) of the function app $functionAppName"
 Get-AzADServicePrincipal -SearchString $functionAppName | Where-Object { $_.DisplayName -eq $functionAppName }
 $msiObjectId = $(Get-AzADServicePrincipal -SearchString $functionAppName | Where-Object { $_.DisplayName -eq $functionAppName }).Id
 
+if ($null -eq $msiObjectId) {
+    throw "Can't find the ObjectId of the MSI of the Function APP"
+}
+
 # Get application and service principal
+Write-Host "The function app will be owner of app object id $appObjectIdThatNeedsRotation"
 Get-AzADApplication -ObjectId $appObjectIdThatNeedsRotation
 Get-AzADApplication -ObjectId $appObjectIdThatNeedsRotation | Get-AzADServicePrincipal
 
